c89348f2c08393a8725ce97230763a23b4da3da7
chrissysemens <chrissysemens@gmail.com>
2025-06-08 19:40:29 +0200

    Disable new arch

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -8,7 +8,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   icon: "./assets/icon.png",
   userInterfaceStyle: "light",
   scheme: "songdrop",
-  newArchEnabled: true,
+  newArchEnabled: false,
   splash: {
     image: "./assets/splash-icon.png",
     resizeMode: "contain",
